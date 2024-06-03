1061caae6b1e8cba870d64865939f31fb41399d2
gautam1103 <mishragautam996@gmail.com>
2024-06-03 23:03:27 +0530

    Added some more concepts of Refrences in the bottom of the code

--- Refrences.cpp
+++ Refrences.cpp
@@ -12,5 +12,12 @@ int main()
 
     ival = 90;
     cout << "New ival = " << ival << ", New Pval = " << pval << endl;
+
+    const int i = 42;
+    const int &r = i;
+
+    const int &r2 = 43;
+    const int &r3 = i * 8;
+    cout << i <<" "<< r <<" " << r2 <<" " << r3<< endl;
     return 0;
 }
